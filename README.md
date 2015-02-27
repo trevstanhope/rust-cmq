982eac7cbe2e0983301f614845d3425c24614fc9
Trevor Stanhope <trevor@W510>
2015-02-27 03:26:35 -0500

    license

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 This is a cargo-style project, so to build, simply run:
 
     cargo build
+
+## Requirements
+* rust 1.0
+* libclang-dev
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// main.rs
+
+// include external crates
+extern crate color;
+extern crate time;
+extern crate cmq;
+
+// use these functions
+use color::{Rgb, ToHsv};
+
+// main
 fn main() {
-    println!("Hello, world!");
+
+    println!("Converting RGB to HSV!");
+    let red = Rgb::new(255u8, 0, 0);
+    println!("HSV: {:?}", red.to_hsv::<f32>());
+    
+    let m = "2";
+    cmq::pretty_print(m);
 }
